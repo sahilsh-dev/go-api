0300f14d6dd719617cb97875542165e59614dbb4
Sahil Sharma <vlsharma713@gmail.com>
2025-04-27 15:26:58 +0530

    Add workout routes

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,19 +5,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/sahilsh-dev/goApi/internal/api"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
-	app := &Application{
-		Logger: logger,
-	}
+	// our store go here
 
+	// our handlers go here
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+	}
 	return app, nil
 }
 
